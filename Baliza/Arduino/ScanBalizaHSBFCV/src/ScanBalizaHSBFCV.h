cc93f5966715697c54e5b63d3eb03bebbecc5da9
yhon tolosa <imartolo86@gmail.com>
2020-04-16 09:29:19 -0500

    pruebas libreria

--- Baliza/Arduino/ScanBalizaHSBFCV/src/ScanBalizaHSBFCV.h
+++ Baliza/Arduino/ScanBalizaHSBFCV/src/ScanBalizaHSBFCV.h
@@ -1,31 +1,34 @@
-#ifndef ScanHSBFCV_h
-#define ScanHSBFCV_h
-/*
-#include "Arduino.h"
+#ifndef ScanBalizaHSBFCV_h
+#define ScanBalizaHSBFCV_h
+
+#include <Arduino.h>
+#include <Ticker.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
+
 #include <DNSServer.h>
 #include <WebServer.h>
 #include <WiFiManager.h>
+
 #include <Hash.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
-#include "BalizaHSBFCV.h"
-#include "base64.h"
-*/
 
-
-class ScanHSBFCV
+ class ScanBalizaHSBFCV
+ 
 {
-
+			
   public:
-    void enviarMensajePost();
-    void InitSystem();
-    void OTA();
-
-	private:
-
+  
+	void enviarMensajePost();
+	void InitSystem();
+	void OTA();
+      
+  private:
+  
 };
-
+  
 #endif
+
+
